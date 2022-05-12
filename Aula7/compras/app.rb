15b706004c13d06acf890fb34c3d00c9435338c5
laryssabiroli <69228830+laryssabiroli@users.noreply.github.com>
2022-05-12 12:20:56 -0300

    Update app.rb

--- Aula7/compras/app.rb
+++ Aula7/compras/app.rb
@@ -9,5 +9,7 @@ produto2 = Produto.new
 produto2.name = "HQ - Flash"
 produto2.price = 130.90
 
-Mercado.new(produto1.name, produto1.price).comprar
-Mercado.new(produto2.name, produto2.price).comprar
+mercado1 = Mercado.new(produto1.name, produto1.price)
+mercado1.comprar
+mercado2 = Mercado.new(produto2.name, produto2.price)
+mercado2.comprar
